950b62c01278ae0af92b2d656ffa1b1da9040c28
debuginn <xianhui.meng.cn@gmail.com>
2020-05-28 18:08:19 +0800

    modify 修改结构逻辑，将处理结果存入 map 中

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module DouBanMyMovie
 
 go 1.13
+
+require github.com/fatih/structs v1.1.0 // indirect
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 // 豆瓣 xml 描述结构体
@@ -14,6 +15,7 @@ type Attributes struct {
 	Channel Channel  `xml:"channel"`
 }
 
+// XML 主题结构拆分
 type Channel struct {
 	Title       string      `xml:"title"`
 	Link        string      `xml:"link"`
@@ -32,32 +34,31 @@ type MovieItem struct {
 	Pubdate     string `xml:"pubDate"`
 }
 
-type MovieFmtItem struct {
-	Title   string
-	Link    string
-	ImgURL  string
-	Pubdate string
-}
-
-func main() {
+// 获取 xml 文件数据
+func getXMLData(url string) (data []byte, err error) {
 	// 读取 xml 文件
 	client := &http.Client{}
-	req, _ := http.NewRequest("GET", "https://www.douban.com/feed/people/debuginn/interests", nil)
+	req, _ := http.NewRequest("GET", url, nil)
 	// 自定义Header
 	req.Header.Set("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)")
 	resp, err := client.Do(req)
-
 	if err != nil {
-		fmt.Printf("open xml file faild, err:%v\n", err)
-		return
+		return nil, err
 	}
 	defer resp.Body.Close() // 关闭文件
+	// 读取所有文件内容保存至 []byte
+	data, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return
+}
 
-	data, readAllErr := ioutil.ReadAll(resp.Body)
-
-	if readAllErr != nil {
-		fmt.Printf("read xml body faild, err:%v\n", readAllErr)
-		return
+func main() {
+	url := "https://www.douban.com/feed/people/debuginn/interests"
+	data, err := getXMLData(url)
+	if err != nil {
+		fmt.Printf("read xml file faild, err:%v\n", err)
 	}
 
 	v := Attributes{}
@@ -66,8 +67,23 @@ func main() {
 		fmt.Printf("xml unmarshal faild, err:%v\n", err)
 	}
 
-	MovieItem := v.Channel.MovieItem
+	movieItem := v.Channel.MovieItem
+	MoviesMap := make(map[int]interface{})
 
-	fmt.Printf("%#v\n", MovieItem)
+	for i := 0; i < len(movieItem); i++ {
+		movie := make(map[string]string)
+		description := strings.Split(movieItem[i].Description, "\"")
+		movie["Title"] = string([]rune(movieItem[i].Title)[2:])
+		movie["Link"] = movieItem[i].Link
+		movie["Description"] = description[7]
+		movie["Pubdate"] = movieItem[i].Pubdate
+
+		MoviesMap[i] = movie
+	}
+
+	//循环输出
+	for k, v := range MoviesMap {
+		fmt.Println(k, v)
+	}
 
 }
